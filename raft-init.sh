656689ea1c910373a29f3b758e545ca350450035
vasa-develop <vasa.develop@gmail.com>
2018-05-05 22:38:15 +0530

    2 node

--- raft-init.sh
+++ raft-init.sh
@@ -13,22 +13,3 @@ cp permissioned-nodes.json qdata/dd4/
 cp keys/key4 qdata/dd4/keystore
 cp raft/nodekey4 qdata/dd4/geth/nodekey
 geth --datadir qdata/dd4 init genesis.json
-
-echo "[*] Configuring node 5"
-mkdir -p qdata/dd5/{keystore,geth}
-cp permissioned-nodes.json qdata/dd5/static-nodes.json
-cp keys/key5 qdata/dd5/keystore
-cp raft/nodekey5 qdata/dd5/geth/nodekey
-geth --datadir qdata/dd5 init genesis.json
-
-echo "[*] Configuring node 6"
-mkdir -p qdata/dd6/{keystore,geth}
-cp permissioned-nodes.json qdata/dd6/static-nodes.json
-cp raft/nodekey6 qdata/dd6/geth/nodekey
-geth --datadir qdata/dd6 init genesis.json
-
-echo "[*] Configuring node 7"
-mkdir -p qdata/dd7/{keystore,geth}
-cp permissioned-nodes.json qdata/dd7/static-nodes.json
-cp raft/nodekey7 qdata/dd7/geth/nodekey
-geth --datadir qdata/dd7 init genesis.json
--- raft-start.sh
+++ raft-start.sh
@@ -10,9 +10,7 @@ echo "[*] Starting Ethereum nodes"
 set -v
 ARGS="--nodiscover --raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum --emitcheckpoints"
 PRIVATE_CONFIG=qdata/c4/tm.ipc nohup geth --datadir qdata/dd4 $ARGS --permissioned --raftport 50404 --rpcport 22003 --port 21003 2>>qdata/logs/4.log &
-PRIVATE_CONFIG=qdata/c5/tm.ipc nohup geth --datadir qdata/dd5 $ARGS --raftport 50405 --rpcport 22004 --port 21004 2>>qdata/logs/5.log &
-PRIVATE_CONFIG=qdata/c6/tm.ipc nohup geth --datadir qdata/dd6 $ARGS --raftport 50406 --rpcport 22005 --port 21005 2>>qdata/logs/6.log &
-PRIVATE_CONFIG=qdata/c7/tm.ipc nohup geth --datadir qdata/dd7 $ARGS --raftport 50407 --rpcport 22006 --port 21006 2>>qdata/logs/7.log &
+
 set +v
 
 echo
