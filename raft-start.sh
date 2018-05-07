eb0336af050b2af7aa02d3a0ddd2b0bde2f4d013
vasa-develop <vasa.develop@gmail.com>
2018-05-07 16:05:16 +0530

    added 3rd node

--- raft-start.sh
+++ raft-start.sh
@@ -9,10 +9,10 @@ echo "[*] Starting Constellation nodes"
 echo "[*] Starting Ethereum nodes"
 set -v
 ARGS="--nodiscover --raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum --emitcheckpoints"
-PRIVATE_CONFIG=qdata/c4/tm.ipc nohup geth --datadir qdata/dd4 $ARGS --permissioned --raftport 50404 --rpcport 22003 --port 21003 2>>qdata/logs/4.log &
+PRIVATE_CONFIG=qdata/c7/tm.ipc nohup geth --datadir qdata/dd7 $ARGS --permissioned --raftport 50407 --rpcport 22006 --port 21006 2>>qdata/logs/7.log &
 
 set +v
 
 echo
 echo "All nodes configured. See 'qdata/logs' for logs, and run e.g. 'geth attach qdata/dd1/geth.ipc' to attach to the first Geth node."
-echo "To test sending a private transaction from Node 3 to Node 7, run './runscript.sh script1.js'"
+echo "To test sending a private transaction, run './runscript.sh script1.js'"
--- runscript.sh
+++ runscript.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-PRIVATE_CONFIG=qdata/c1/tm.ipc geth --exec "loadScript(\"$1\")" attach ipc:qdata/dd1/geth.ipc
+PRIVATE_CONFIG=qdata/c7/tm.ipc geth --exec "loadScript(\"$1\")" attach ipc:qdata/dd7/geth.ipc
